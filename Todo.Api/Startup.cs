349809f6512167dc6c3360b88e8737df1052312c
yonnybeyene <yonnybeyene@github.com>
2020-04-15 21:05:14 -0400

    add file

--- Todo.Api/Startup.cs
+++ Todo.Api/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Todo.Api.Services;
 
 namespace Todo.Api
 {
@@ -26,6 +27,7 @@ namespace Todo.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddScoped<ITodoRepository, TodoRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
