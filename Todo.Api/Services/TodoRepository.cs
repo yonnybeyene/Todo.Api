be5089345ea9681664b27ffbcbaed9b9f660134d
yonnybeyene <yonnybeyene@github.com>
2020-04-15 20:39:42 -0400

    add file

--- Todo.Api/Services/TodoRepository.cs
+++ Todo.Api/Services/TodoRepository.cs
@@ -1,15 +1,19 @@
 using System.Collections.Generic;
 using Todo.Api.Models;
 
+
 namespace Todo.Api.Services
 {
-    public class TodoRepository
+    
+    public class TodoRepository : ITodoRepository
     {
         private List<TodoItem> _items = new List<TodoItem>()
         {
             new TodoItem{ Id = 1, Action="Thing one.", IsDone=true},
             new TodoItem{ Id = 2, Action="Thing two.", IsDone=false},
         };
+
+    
 
         public IEnumerable<TodoItem> GetAllTodoItems()
         {
